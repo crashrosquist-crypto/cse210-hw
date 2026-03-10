3f545ae97b9c4e8cb11a1e7a7488b1e12d095aa2
crashrosquist-crypto <crosquist@byupathway.edu>
2026-03-09 19:38:38 -0600

    Assignment

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Apple";
+        job2._startYear = 2015;
+        job2._endYear = 2025;
+
+        // job1.Display();
+        // job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        // Console.WriteLine(myResume._jobs[0]._jobTitle);
+
+        myResume.Display();
     }
 }
