cc64db0d7600f7325ecd71117a626ec659f5460a
crashrosquist-crypto <crosquist@byupathway.edu>
2026-03-30 13:00:58 -0600

    IM ON FIRE BABY

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+
+        MathAssignment a1 = new MathAssignment("Crash Rosquist", "Calculus", "Calc-R3", "10-15");
+        Console.WriteLine(a1.GetHomeworkList());
+        // Just some extra practice
     }
 }
